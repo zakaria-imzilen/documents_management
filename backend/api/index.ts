99518b0385ffd96298c1fdaeaaafb3688d78899b
Zakaria Imzilen <zakariyaimzilen@gmail.com>
2024-02-05 00:18:15 +0100

    Update

---
+++ backend/api/index.ts
@@ -0,0 +1,56 @@
+import express from "express";
+import morgan from "morgan";
+import { config } from "dotenv";
+import cors from "cors";
+import "./services/starategy.service";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import passport from "passport";
+import badRequest from "../src/middlewares/badRequest";
+import authRouter from "../src/routes/auth.route";
+import documentRouter from "../src/routes/document.route";
+import dbConnecting from "../src/config/db";
+import errorMiddleware from "../src/middlewares/error";
+
+config({ path: __dirname + "/environments/.env" });
+
+const app = express();
+
+declare global {
+  namespace Express {
+    interface Request {
+      fileName?: string;
+      filePath?: string;
+    }
+  }
+}
+
+app.use(morgan("dev"));
+app.use(session({ secret: "njkds2398dsf" }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  console.log("Request Body: ", req.body);
+  console.log("Request Cookies: ", req.cookies);
+  next();
+});
+
+app.use("/api/document", dbConnecting, documentRouter);
+app.use("/api/auth", dbConnecting, authRouter);
+
+app.use(badRequest);
+app.use(errorMiddleware);
+
+export default app;
