b4cf9ae27f4b4126efa026dc01db3320cf3e63fc
Zakaria Imzilen <zakariyaimzilen@gmail.com>
2024-02-05 00:29:11 +0100

    Change Logs paths

--- backend/config/logs/anyOtherError.ts
+++ backend/config/logs/anyOtherError.ts
@@ -1,10 +1,10 @@
 import { createLogger, format, transports } from "winston";
 
 export default createLogger({
-    format: format.json(),
-    level: "error",
-    transports: [
-        new transports.Console(),
-        new transports.File({ filename: "../../../logs/any_other_error.log" }),
-    ],
+  format: format.json(),
+  level: "error",
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: "./records/any_other_error.log" }),
+  ],
 });
